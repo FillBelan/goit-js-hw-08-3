c47da85d3d8ff8c93e58f56e27cbb47e419e3861
FilBelan <filbelan@gmail.com>
2022-12-13 15:15:26 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const images = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  images.insertAdjacentHTML(
+    "afterbegin",
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  );
+});
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox');
+
