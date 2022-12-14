2ddb8a0586383218add778e78e8d0aade866b5a7
FilBelan <filbelan@gmail.com>
2022-12-14 11:57:41 +0200

    tasck 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+// import '@vimeo/player/dist/player.js';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const durationTime = _.throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+const dataTime = localStorage.getItem('videoplayer-current-time');
+// console.log(dataTime);
+
+player.on('timeupdate', durationTime);
+
+player
+  .setCurrentTime(dataTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  const check = _.add(2, 3);
