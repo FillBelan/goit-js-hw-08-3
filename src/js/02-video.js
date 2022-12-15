0a857270688762d73d4633430e560522ce6415c7
FilBelan <filbelan@gmail.com>
2022-12-15 15:40:05 +0200

    task 8.3 final 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 // import '@vimeo/player/dist/player.js';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const durationTime = _.throttle(function (data) {
+const durationTime = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000);
+};
 
 const dataTime = localStorage.getItem('videoplayer-current-time');
 // console.log(dataTime);
 
-player.on('timeupdate', durationTime);
+player.on('timeupdate', throttle(durationTime, 1000));
 
 player
   .setCurrentTime(dataTime)
@@ -26,4 +27,3 @@ player
     }
   });
 
-  const check = _.add(2, 3);
